4d49bfd3a17ccd2284d07b913adb67bb1a113e13
Nicolas Lu <nwlu@hotmail.com>
2025-07-28 20:18:37 -0500

    labs

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -1,25 +1,102 @@
 package labs_examples.objects_classes_methods.labs.methods;
 
 public class MethodTraining {
-//Exercise 1
-    public static int muliply(int a, int b){
+    //Exercise 1
+    public static int muliply(int a, int b) {
         return a * b;
     }
 
-    public static int multiply(double a, double b){
+    public static int multiply(double a, double b) {
         return (int) ((int) a * b);
     }
 
-    public static int multiply(int a, int b, int c){
+    public static int multiply(int a, int b, int c) {
         return a * b * c;
     }
 
-//Exercise 2 Pass by Value
+    //Exercise 2 Pass by Value
+    public static int passByValue(int zebra) {
+        zebra = zebra * 10;
+        System.out.println(zebra);
+        return zebra;
+    }
 
+    public static void main(String[] args) {
+        int y = 5;
+        y = passByValue(y);
+        System.out.println(y);
 
+        Cat cat = new Cat();
+        cat.name = "Sylvester";
 
+        passByReference(cat);
+        System.out.println(cat.name);
 
+        int a = 5;
+        int b = 15;
+        int c = 3;
+        int d = 25;
 
-//Exercise 2 Pass by Reference
+        int num = numbers(a, b, c, d);
+        System.out.println("The largest number is " + num);
+
+        String test = "Java is getting easier!";
+        System.out.println("Consonants: " + consonants(test));
+    }
+
+    //Exercise 2 Pass by Reference
+    public static void passByReference(Cat cat) {
+        cat.name = "Garfield";
+    }
+
+    //Create a method that will return the largest of 4 numbers (all of which are passed in as arguments)
+    public static int numbers(int a, int b, int c, int d) {
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
+        return max;
+    }
+        int testNumber = 29;
+        if(primeNumber(testNumber)){
+            System.out.println(testNumber + " is a prime number.");
+        } else {
+            System.out.println(testNumber + " is not a prime number.");
+        }
+        
+
+    //Write a method to count all consonants (the opposite of vowels) in a String
+    public static int consonants(String letters) {
+        int count = 0;
+        letters = letters.toLowerCase();
+
+        for (int i = 0; i < letters.length(); i++) {
+            char ch = letters.charAt(i);
+            if (Character.isLetter(ch) && !"aeiou".contains(String.valueOf(ch))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Write a method that will determine whether or not a number is prime
+    public static boolean primeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // Divisible by something other than 1 and itself
+            }
+        }
+        return true; // No divisors found, it's prime
+    }
 
 }
--- src/labs_examples/objects_classes_methods/labs/methods/ObjectComposition.java
+++ src/labs_examples/objects_classes_methods/labs/methods/ObjectComposition.java
@@ -1,47 +1,49 @@
 package labs_examples.objects_classes_methods.labs.methods;
 
 public class ObjectComposition {
-    class Main{
-        public static void main(String[] args){
-            // create a composite object here
-            // after creating your new classes below
-            // refer to the previous example for guidance
-            Paddle paddle = new Paddle("Sypik", 8);
-            Ball ball = new Ball("Vulcan ProV");
 
-            Bag mybag = new Bag("Carbon", paddle, ball);
+    public static void main(String[] args) {
+        // create a composite object here
+        // after creating your new classes below
+        // refer to the previous example for guidance
+        Paddle paddle = new Paddle("Sypik", 8);
+        Ball ball = new Ball("Vulcan ProV");
 
-        }
-    }
-
-    // create your first class below this line
-    class Paddle{
-        String name;
-        int weight;
-
-        public Paddle(String name, int weight){
-            this.name = name;
-            this.weight = weight;
-        }
-    }
-    // create your second class below this line
-    class Ball{
-        String brand;
-
-        public Ball(String brand){
-            this.brand = brand;
-        }
-    }
-    // create your third class below this line
-    class Bag{
-        String brand;
-        Paddle name;
-        Ball brand;
-
-        public Bag(String brand, String name, String brand){
-            this.brand = brand;
-            this.name = Paddle.name;
-            this.Ball.brand = brand;
-        }
+        Bag mybag = new Bag("Carbon", paddle, ball);
+        System.out.println(mybag.paddle.name);
     }
 }
+
+// create your first class below this line
+class Paddle {
+    String name;
+    int weight;
+
+    public Paddle(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+}
+
+// create your second class below this line
+class Ball {
+    String brand;
+
+    public Ball(String brand) {
+        this.brand = brand;
+    }
+}
+
+// create your third class below this line
+class Bag {
+    String brand;
+    Paddle paddle;
+    Ball ball;
+
+    public Bag(String brand, Paddle paddle, Ball ball) {
+        this.brand = brand;
+        this.paddle = paddle;
+        this.ball = ball;
+    }
+}
+
