7b54c4c17559268dfafcf32543d49f6d660d73a4
Nicolas Lu <nwlu@hotmail.com>
2025-08-14 18:54:12 -0500

    exceptions

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -6,4 +6,17 @@ package labs_examples.exception_handling.labs;
  *      1) Demonstrate a try/catch.
  *
  */
+class Try {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int c = 0;
 
+        try{
+            int x = a / c;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Exception caught " + e.toString());
+        }
+    }
+}
--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -6,5 +6,21 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch with multiple catch statements.
  *
  */
+class TryAgain {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int c = 0;
 
+        try{
+            int x = a / c;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Exception caught " + e.toString());
+        }
+        catch (Exception e) {
+            System.out.println("You can't divide a number by 0");
+        }
+    }
+}
 
