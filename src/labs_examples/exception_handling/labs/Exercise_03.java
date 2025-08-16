cbeeab62a2fb94aa4c264802d5c34ae3f3cafbb8
Nicolas Lu <nwlu@hotmail.com>
2025-08-16 11:17:46 -0500

    exception handling

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -1,8 +1,33 @@
 package labs_examples.exception_handling.labs;
 
+import videos_source_code.arrays.ArrayLists;
+
+import java.util.ArrayList;
+
 /**
  * Exception Handling Exercise 3:
  *
  *      Demonstrate a try/catch/finally block.
  *
  */
+class TryAgainAgain {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int c = 0;
+
+        ArrayList
+        try{
+            int x = a / b;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Exception caught " + e.toString());
+        }
+        catch (Exception e) {
+            System.out.println("You can't divide a number by 0");
+        }
+        finally {
+            System.out.println("This method will always execute");
+        }
+    }
+}
